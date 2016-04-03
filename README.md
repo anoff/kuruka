eb99a5a694e1626dadd53c9bfbdd72e13eea250d
anoff <offispam@gmail.com>
2016-04-03 03:37:51 -0700

    updated lighting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# kuruka
+kuruka
+=====
+
 in-browser game inspired by race the sun using THREE.js
--- web/map.js
+++ web/map.js
@@ -1,60 +1,62 @@
-var Map = (function (THREE) {
-	const GROUND_LEVEL = -10;
-	function generateSpike(radius, height) {
-		var geometry = new THREE.CylinderGeometry( 0, radius, height, 8 );
-		var material = new THREE.MeshLambertMaterial( {color: 0xc8c8c8} );
-		var spike = new THREE.Mesh( geometry, material );
-		spike.position.y = GROUND_LEVEL;
-		return spike;
-	}
+var Map = (function(THREE) {
+  var GROUND_LEVEL = -10;
+  var COLOR_SKY = 0x0000ff;
+  var COLOR_GROUND = 0xc8c8c8;
+  var COLOR_OBJECT = 0xc8c8c8;
 
-	function createWorld() {
-        var ambient	= new THREE.AmbientLight( 0x444444 );
-		scene.add( ambient );
-		
-		var light	= new THREE.DirectionalLight( 0x4444cc, 2 );
-		light.position.set( 1, -1, 1 ).normalize();
-		scene.add( light );
+  function generateSpike(radius, height) {
+    var geometry = new THREE.CylinderGeometry(0, radius, height, 8);
+    var material = new THREE.MeshLambertMaterial({ color: COLOR_OBJECT });
+    var spike = new THREE.Mesh(geometry, material);
+    spike.position.y = GROUND_LEVEL;
+    return spike;
+  }
 
-		var spotLight	= new THREE.SpotLight( 0x00FF00 );
-		spotLight.target.position.set( 0, 2, 0 );
-		spotLight.position.set( 0, 200, 1000 );
-		spotLight.castShadow		= true;
-		scene.add( spotLight );	
+  function createWorld() {
+    /*
+    var ambient = new THREE.AmbientLight(0x444444);
+    scene.add(ambient);
 
-/*
-        // add ground
-        var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
-        var groundMat = new THREE.MeshPhongMaterial( { 
-			color		: 0x66aa66,
-			specular	: 0x888888,
-			shading		: THREE.SmoothShading
-		} );
-        var ground = new THREE.Mesh( groundGeo, groundMat );
-        ground.scale.multiplyScalar(3);
-        ground.rotation.x = -Math.PI/2;
-        ground.position.y = GROUND_LEVEL;
-        ground.receiveShadow = true;
-        scene.add( ground );
+    var light = new THREE.DirectionalLight(0x4444cc, 2);
+    light.position.set(1, -1, 1).normalize();
+    scene.add(light);
+
+    var spotLight = new THREE.SpotLight(0x00FF00);
+    spotLight.target.position.set(0, 0, 0);
+    spotLight.position.set(0, 200, 1000);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
 */
-var geometry	= new THREE.CubeGeometry( 1000, 0.2, 1000);
-	var material	= new THREE.MeshPhongMaterial({
-		ambient		: 0x444444,
-		color		: 0x66aa66,
-		shininess	: 150, 
-		specular	: 0x888888,
-		shading		: THREE.SmoothShading
-	});
-	var ground		= new THREE.Mesh( geometry, material );
-	ground.scale.multiplyScalar(3);
-	ground.position.y		= GROUND_LEVEL;
-	scene.add( ground );
+    var directionalLight = new THREE.DirectionalLight( COLOR_SKY, 1 );
+    directionalLight.target.position.set( 0, 0, 0);
+    directionalLight.castShadow = true;
+    scene.add( directionalLight );
 
-	ground.castShadow	= false;
-	ground.receiveShadow	= true;
- 	}
-	return {
-		generateSpike: generateSpike,
-		createWorld: createWorld
-	};
+    var sphere = new THREE.SphereGeometry( 0.5, 16, 8 );
+    var light1 = new THREE.PointLight( 0xff00ff, 0, 50 );
+    light1.position.set( 0, 50, 1000 );
+    light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff00ff } ) ) );
+    scene.add( light1 );
+
+    var light = new THREE.HemisphereLight( COLOR_SKY, COLOR_GROUND, 1 );
+    light.position.set( 0, 500, 0 );
+    scene.add( light );
+
+    // add ground
+    var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
+    var groundMat = new THREE.MeshPhongMaterial( {
+      color    : COLOR_GROUND,
+      shading    : THREE.SmoothShading
+    } );
+    var ground = new THREE.Mesh( groundGeo, groundMat );
+    ground.scale.multiplyScalar(3);
+    ground.rotation.x = -Math.PI/2;
+    ground.position.y = GROUND_LEVEL;
+    ground.receiveShadow = true;
+    scene.add( ground );
+  }
+  return {
+    generateSpike: generateSpike,
+    createWorld: createWorld
+  };
 })(THREE);
